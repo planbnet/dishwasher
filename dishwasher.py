146d2865846ecd8986ba88a740620fce90d18604
Felix Rotthowe <felix@planbnet.org>
2022-08-17 21:50:02 +0200

    fix timezone and slow starts

--- dishwasher.py
+++ dishwasher.py
@@ -23,42 +23,48 @@ with InfluxDBClient(url="http://openhab:8086", token="openhab", debug=False) as 
 df = df.loc[ df["item"] == "PowerConsumption", ["timestamp", "value" ] ]
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 df.set_index('timestamp', inplace=True)
+df.index = df.index.tz_convert("Europe/Berlin")
 
 df = df.resample("60s").max()
 difference = df.diff(axis=0).dropna();
 possible_ends = difference[ difference < -800 ].dropna()
 
 if len(possible_ends) == 0:
+    print(f"No possible ends found in timeframe - {lookback_range}")
     exit()
 
 possible_matches = []
+
 for possible_end in list(possible_ends.index):
     possible_start = possible_end - pd.Timedelta(minutes=90)
     series = df.loc[possible_start : possible_end]
 
-    difference = series.diff(axis=0).dropna();
+    difference = series.fillna(method='ffill').diff(axis=0).dropna()
     idx = difference[ difference > 800 ].first_valid_index()
-    series = series[idx:]
+    match = series[idx:]
 
     # simple heuristic to prefilter:
     #   all samples must be present
     #   at least 1 kWh must be consumed in the timeframe
-    #   the first 10 minutes must be > 1500 W
+    #   the first 9 minutes must be > 1500 W
 
-    kWh = series["value"].sum() / 60 / 1000
+    kWh = match["value"].sum() / 60 / 1000
 
-    if ( len(series) >= 60 and kWh > 1.5 and (series.iloc[0:10]["value"] > 1500).all() ):
+    if ( len(match) >= 60 and kWh > 1.5 and (match.iloc[1:8]["value"] > 1500).all() ):
         # restrict sample size, index by number (minute)
-        series = series.reset_index(drop=True)
+        match = match.reset_index(drop=True)
 
         # limit sample count and append additional samples if there aren't enough (repeat last value)
         last_value = df.iloc[-1]["value"]
-        series = series[:90]
-        series = series.reindex(range(90), fill_value=last_value)
+        match = match[:90]
+        match = match.reindex(range(90), fill_value=last_value)
 
-        row = series.transpose()
+        row = match.transpose()
 
         possible_matches.append( row )
+        print(f"Added {idx} - {possible_end} with len {len(match)} kWh {kWh}")
+    else:
+        print(f"Skipped {possible_start} - {possible_end} starting at {idx} with len {len(match)} kWh {kWh}")
 
 if len(possible_matches) == 0:
     exit()
@@ -72,5 +78,5 @@ for i in range(0, len(pred)):
     print(f"dishwasher probability: {probability:.3f}")
 
 if (pred == "1").any(): 
-    #publish.single("dishwasher", "done", hostname="192.168.10.222")
+    publish.single("dishwasher", "done", hostname="192.168.10.222")
     print(f"found a dishwasher")
